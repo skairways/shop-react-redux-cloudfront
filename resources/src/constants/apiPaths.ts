cf54fcc2564274a85f86d16f7ff7c7a685a2c85c
Sultonbek <sultonbek_nazarov@epam.com>
2025-05-03 17:34:10 +0500

    task-3

--- resources/src/constants/apiPaths.ts
+++ resources/src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://0agkk3p5t5.execute-api.us-east-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://0agkk3p5t5.execute-api.us-east-1.amazonaws.com/prod",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
